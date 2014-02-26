1d3e795ec235356ba3b3666127645e7b4e553bca
josepborrellsanchez <josepborrellsanchez@iesebre.com>
2014-02-26 21:44:28 +0100

    no va

---
+++ src/com/iesebre/DAM2/projectemusiqueta/MainActivity.java
@@ -0,0 +1,108 @@
+package com.iesebre.DAM2.projectemusiqueta;
+
+import com.iesebre.DAM2.projectemusiqueta.MusicService.ServiceBinder;
+
+import android.os.Bundle;
+import android.os.IBinder;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private boolean mIsBound = false;
+	private MusicService mServ;
+	private ServiceConnection Scon =new ServiceConnection(){
+
+		public void onServiceConnected(ComponentName name, IBinder
+	     service) {
+			ServiceBinder binder = (ServiceBinder) service;
+			mServ = binder.getService();
+		}
+
+		public void onServiceDisconnected(ComponentName name) {
+			mServ = null;
+		}
+		};
+
+		void doBindService(){
+	 		bindService(new Intent(this,MusicService.class),
+					Scon,Context.BIND_AUTO_CREATE);
+			mIsBound = true;
+		}
+
+		void doUnbindService()
+		{
+			if(mIsBound)
+			{
+				unbindService(Scon);
+	      		mIsBound = false;
+			}
+		}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		doBindService();
+		
+		Button button1 = (Button) findViewById(R.id.button1);
+		Button button2 = (Button) findViewById(R.id.button2);
+		Button button3 = (Button) findViewById(R.id.button3);
+		Button button4 = (Button) findViewById(R.id.button4);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View arg0)
+		    {
+		    	Intent music = new Intent(getBaseContext(), MusicService.class);
+		    	//music.setClass(MainActivity.this,MusicService.class);
+		    	startService(music);
+		    	//mServ.startMusic();
+		    }
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View arg0)
+		    {
+		    	Intent music = new Intent();
+		    	music.setClass(MainActivity.this,MusicService.class);
+		    	mServ.stopMusic();
+		    }
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View arg0)
+		    {
+		    	Intent music = new Intent();
+		    	music.setClass(MainActivity.this,MusicService.class);
+		    	mServ.pauseMusic();
+		    }
+		});
+		
+		button4.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View arg0)
+		    {
+		    	Intent music = new Intent();
+		    	music.setClass(MainActivity.this,MusicService.class);
+		    	mServ.resumeMusic();
+		    }
+		});
+	}
+	
+	
+	
+	
+		
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
